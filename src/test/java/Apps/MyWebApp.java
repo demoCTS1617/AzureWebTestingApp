df7f1c578da817b9399ee555e60f3872268926c5
demoCTS1617 <demo.nilesh@gmail.com>
2023-06-23 16:06:16 +0530

    first commit

---
+++ src/test/java/Apps/MyWebApp.java
@@ -0,0 +1,24 @@
+package Apps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyWebApp {
+  @Test
+  public void verifyTitle() throws InterruptedException {
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://myazuredemo.azurewebsites.net/mywebProject/");
+	  
+	  String title = driver.getTitle();
+	  
+	  System.out.println("Title is "+title);
+
+	  Assert.assertEquals(title, "Login Page");
+	  
+	  Thread.sleep(2000);
+	  
+	  driver.close();
+  }
+}
