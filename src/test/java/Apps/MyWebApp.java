6bb10ebba0bf6de37dfdb3f05e8f5fe83651413b
demoCTS1617 <83628915+demoCTS1617@users.noreply.github.com>
2023-06-26 12:48:55 +0530

    Update import comment MyWebApp.java

--- src/test/java/Apps/MyWebApp.java
+++ src/test/java/Apps/MyWebApp.java
@@ -2,7 +2,7 @@ package Apps;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+//import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MyWebApp {
